a147f3a4bbfcfcd5f55872ed75a86588a8557783
Badger <unconfigured@null.spigotmc.org>
2020-11-16 23:33:11 +0000

    Fix

--- BadgerTrafficDensity/BadgerTrafficDensity.cs
+++ BadgerTrafficDensity/BadgerTrafficDensity.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Newtonsoft;
 using Newtonsoft.Json.Linq;
 using CitizenFX.Core;
 using static CitizenFX.Core.Native.API;
@@ -10,26 +11,26 @@ namespace BadgerTrafficDensity
         float pedFrequency;
         float vehFrequency;
 
-        string jsonConfig = LoadResourceFile("BadgerScripts", "config/config.json");
-
+        string jsonConfig = LoadResourceFile("BadgerTrafficDensity", "config/config.json");
+        
         Newtonsoft.Json.JsonObjectAttribute json = new Newtonsoft.Json.JsonObjectAttribute();
 
         public BadgerTrafficDensity()
         {
             Tick += onTick;
 
+            // Load config values
             JObject o = JObject.Parse(jsonConfig);
-            pedFrequency = (float)o.SelectToken("pedTrafficFrequency.pedFrequency");
-            vehFrequency = (float)o.SelectToken("pedTrafficFrequency.vehicleFrequency");
-            Debug.WriteLine("[BadgerTrafficDensity Config:]" + "\n" + "pedFrequency: " + pedFrequency + "\n" + "vehicleFrequency: " + vehFrequency);
+            pedFrequency = (float)o.SelectToken("badgerTrafficDensity.pedFrequency");
+            vehFrequency = (float)o.SelectToken("badgerTrafficDensity.vehicleFrequency");
         }
         private async Task onTick()
         {
-            // Ped Density
+            // Set Ped Density
             SetPedDensityMultiplierThisFrame(pedFrequency);
             SetScenarioPedDensityMultiplierThisFrame(pedFrequency, pedFrequency);
 
-            // Traffic Density
+            // Set Traffic Density
             SetVehicleDensityMultiplierThisFrame(vehFrequency);
             SetRandomVehicleDensityMultiplierThisFrame(vehFrequency);
             SetParkedVehicleDensityMultiplierThisFrame(vehFrequency);
